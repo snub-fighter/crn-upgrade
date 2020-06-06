2174e0a65bac28ea230679ecacbe54949b47f2ec
snub-fighter <demers.nathan@gmail.com>
2020-06-05 23:10:15 -0600

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -4,21 +4,20 @@ echo " You will be prompted for your password "
 echo " Ensure you are not root - USE SUDO USER "
 
 
-sudo rm casinocoind* 
+rm casinocoind* 
 
-sudo systemctl stop casinocoind.service
+systemctl stop casinocoind.service
 
-sudo rm /usr/bin/casinocoind && sudo rm /opt/casinocoind/bin/casinocoind
+rm /usr/bin/casinocoind && sudo rm /opt/casinocoind/bin/casinocoind
 
-sudo wget https://github.com/casinocoin/casinocoind/releases/download/v4.0.6/casinocoind-18
+wget https://github.com/casinocoin/casinocoind/releases/download/v4.0.6/casinocoind-18
 
-sudo mv casinocoind-18 casinocoind
+mv casinocoind-18 casinocoind
 
-sudo chmod 777 casinocoind
+chmod 777 casinocoind
 
-sudo cp casinocoind /usr/bin/ && sudo cp casinocoind /opt/casinocoind/bin/
+cp casinocoind /usr/bin/ && sudo cp casinocoind /opt/casinocoind/bin/
 
-sudo systemctl start casinocoind.service
-
-sudo rm casinocoind
+systemctl start casinocoind.service
+rm casinocoind
 
